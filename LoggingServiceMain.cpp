111a7f727b2f23f4b09ec687cf72c0b756de7e33
Patrick Bardo <dr.p.bardo@gmail.com>
2020-02-17 22:07:14 -0500

    Renaming

---
+++ LoggingServiceMain.cpp
@@ -0,0 +1,140 @@
+#include <string>
+#include <iostream>
+
+
+#include <boost/interprocess/ipc/message_queue.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+//#include "Request.hpp"
+//#include "Log.hpp"
+#include "LoggingService.hpp"
+
+// TODO: This is defined twice!!! BADDD
+#define BUFFER_SIZE 5
+
+using namespace boost::interprocess;
+
+/*
+// TODO: Unit test
+Request receiveMessage(message_queue * mq)
+{
+    Request me;
+    message_queue::size_type recvd_size;
+    unsigned int priority;
+
+
+
+    std::stringstream iss;
+    std::string serialized_string;
+    serialized_string.resize(MAX_SIZE);
+
+    while (!mq->try_receive(&serialized_string[0], MAX_SIZE, recvd_size, priority)){
+        usleep(0);
+    }
+
+    //mq.try_receive(&serialized_string[0], MAX_SIZE, recvd_size, priority);
+    iss << serialized_string;
+
+    boost::archive::text_iarchive ia(iss);
+    ia >> me;
+    return me;
+}
+
+// TODO: Unit test
+void dumpLogs(Log * logs, int i, int logLevel)
+{
+    // TODO: Deal with writing to to text file here
+    for (int j=i; j < BUFFER_SIZE+i; ++j){
+        if (logs[j%BUFFER_SIZE].getLogLevel() >= logLevel){
+            std::cout << logs[j%BUFFER_SIZE].getCurrentTime() << " "
+                      << logs[j%BUFFER_SIZE].getClientId() << " "
+                      << logs[j%BUFFER_SIZE].getLogLevelPrint()<< " : "
+                      << logs[j%BUFFER_SIZE].getMessage()
+                      << '\n';
+        }
+
+    }
+}
+*/
+
+int main ()
+{
+    try
+    {
+
+        message_queue mq
+        (
+           open_or_create,
+           "mq",
+           100,
+           MAX_SIZE
+        );
+
+
+        Log logs [BUFFER_SIZE];
+        int i = 0;
+
+        LoggingService loggingService;
+
+        while (true){
+
+            Request me = loggingService.receiveMessage(&mq);
+
+            // Dev command for closing the service
+            if (me.message == "exit()")
+            {
+                break;
+            }
+
+            //TODO
+            //std::cout << "Test action value: " << me.action << '\n';
+
+            // Log action
+            if (me.action == 0) {
+                //
+                logs[i%BUFFER_SIZE].setLog(me.clientId, me.logLevel, me.message);
+                ++i;
+
+            // Dump action
+            } else if (me.action == 1) {
+                std::cout << "Starting dump" << '\n';
+                //TODO: make a seperate function
+                loggingService.dumpLogs(logs, i, me.logLevel);
+                /*
+                for (int j=i; j < std::size(logs)+i; ++j){
+                    if (logs[j%BUFFER_SIZE].getLogLevel() >= me.logLevel){
+                        std::cout << logs[j%BUFFER_SIZE].getCurrentTime() << " "
+                                  << logs[j%BUFFER_SIZE].getClientId() << " "
+                                  << logs[j%BUFFER_SIZE].getLogLevelPrint()<< " : "
+                                  << logs[j%BUFFER_SIZE].getMessage()
+                                  << '\n';
+                    }
+
+                }
+                */
+
+            // Clear action
+            } else {
+                loggingService.clearLogs(logs,i);
+                /*
+                std::cout << "Starting clear" << '\n';
+                for (int j=i; j < std::size(logs)+i; ++j){
+                    logs[j%BUFFER_SIZE].setLog();
+                }
+                */
+            }
+
+
+
+
+
+        }
+
+    }
+    catch(interprocess_exception &ex)
+    {
+        std::cerr << ex.what() << '\n';
+    }
+
+    message_queue::remove("mq");
+}
--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 
-output: LoggingClient receive.o
+output: LoggingClient LoggingService
 	#g++ send.o receive.o -o output
 
 LoggingClient: LoggingClientMain.cpp
 	g++ -std=c++17 LoggingClientMain.cpp LoggingClient.cpp -o LoggingClient -L/usr/local/Cellar/boost/1.72.0/lib/ -lboost_serialization
 
-receive.o: receive.cpp
-	g++ -std=c++17 receive.cpp LoggingService.cpp Log.cpp -o receive -L/usr/local/Cellar/boost/1.72.0/lib/ -lboost_serialization
+LoggingService: LoggingServiceMain.cpp
+	g++ -std=c++17 LoggingServiceMain.cpp LoggingService.cpp Log.cpp -o LoggingService -L/usr/local/Cellar/boost/1.72.0/lib/ -lboost_serialization
 
 clean:
-	rm LoggingClient receive
+	rm LoggingClient LoggingService
